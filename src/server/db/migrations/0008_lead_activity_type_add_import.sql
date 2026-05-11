441a977f3ae0917a3c0ee87174808758fc770977
Moritz Hecht <moritz-hecht@gmx.net>
2026-05-11 11:06:53 +0200

    fix migration

--- src/server/db/migrations/0008_lead_activity_type_add_import.sql
+++ src/server/db/migrations/0008_lead_activity_type_add_import.sql
@@ -1,1 +1,12 @@
+DO
+$$
+BEGIN
+  IF
+EXISTS (
+    SELECT 1
+    FROM pg_type
+    WHERE typname = 'lead_activity_type'
+  ) THEN
 ALTER TYPE "lead_activity_type" ADD VALUE IF NOT EXISTS 'import';
+END IF;
+END $$;
