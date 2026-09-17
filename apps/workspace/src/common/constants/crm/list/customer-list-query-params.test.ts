e8da5c95c87d03486beb58b5cd5b280aae6ad05c
Moritz Hecht <moritz-hecht@gmx.net>
2026-09-17 14:08:24 +0200

    test(workspace): cover cockpit url parameter

--- apps/workspace/src/common/constants/crm/list/customer-list-query-params.test.ts
+++ apps/workspace/src/common/constants/crm/list/customer-list-query-params.test.ts
@@ -13,6 +13,7 @@ describe("CustomerListQueryParam", () => {
       "archived",
       "mode",
       "edit",
+      "cockpit",
       "search",
     ]);
     expect(CUSTOMER_LIST_QUERY_PARAM_VALUES).toEqual(
